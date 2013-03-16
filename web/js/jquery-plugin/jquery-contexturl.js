e39254e63116da019d267b5719e9fa0406da1e98
Yunseok Choi <ethdemor@gmail.com>
2013-03-16 14:09:04 +0900

    Context URL을 추가하는 jQuery 플러그인 추가 및 메인 페이지에서 사용.

---
+++ web/js/jquery-plugin/jquery-contexturl.js
@@ -0,0 +1,52 @@
+(function($, undefined) {
+	$.initContextUrl = function() {
+		if ( CONTEXT_PATH == null || CONTEXT_PATH === "" ) {
+			throw "CONTEXT_PATH가 필요합니다";
+		} else if ( CONTEXT_PATH === "/" ) {
+			return;
+		}
+		
+		// create 이벤트가 트리거 되면 동작하는 코드
+		/* $("body [href], body [src], body [action]").on("click", function(e) {
+			e.preventDefault();
+			
+			var $this = $(this);
+			setContextPath($this);
+			
+			return true;
+		}); */
+		
+		$("body [href], body [src], body [action]").each(function(index, value) {
+			var $this = $(this);
+			setContextPath($this);
+		});
+	};
+	
+	$.fn.contextUrl = function() {
+		this.each(function(index, value) {
+			$(this).find("[href], [src], [action]").each(function() {
+				setContextPath($(this));
+			});
+			
+			setContextPath($(this));
+		});
+	};
+	
+	function setContextPath($this) {
+		var raw = $this[0],
+			attribute = raw.attributes.src || raw.attributes.action || raw.attributes.href,
+			type = attribute == null ? null : attribute.name,
+			oldPath = attribute == null ? null : attribute.value;
+		
+		if ( attribute == null || oldPath.startsWith('http://') || oldPath.startsWith('https://') || oldPath.startsWith(CONTEXT_PATH) ) {
+			return;
+		}
+		
+		$this.attr(type, getPath(oldPath));
+	}
+	
+	function getPath(oldPath) {
+		var withContextPath = CONTEXT_PATH + oldPath;
+		return withContextPath.replace(/\/\//g, '/');
+	}
+})(jQuery);
--- web/js/teamplex-main.js
+++ web/js/teamplex-main.js
@@ -1,5 +1,5 @@
 require.config({
-	baseUrl: PREFIX + 'js',
+	baseUrl: CONTEXT_PATH + 'js',
     paths: {
     	form: 'jquery-plugin/jquery.form',
     	plugins: 'jquery-plugin',
@@ -15,28 +15,13 @@ require.config({
 require(['jquerypp',
          'bootstrap',
          'underscore',
-         'holder',
          'common/teamplex-nav',
          'common/teamplex-post',
          'common/teamplex-common'], function() {
 	
 	$(document).ready(function() {
 		
-		$("body").ezBgResize({
-			img: getBackgroundImageUrl()
-		});
-		
-		function getBackgroundImageUrl() {
-			var url = "";
-			if (parseInt(Math.random() * 2) == 1) {
-				url = "img/living.social.street" + parseInt(Math.random() * 9 + 1);
-			} else {
-				url = "img/twitter-cover" + parseInt(Math.random() * 9 + 1);
-			}
-			
-			return PREFIX + url + ".jpg";
-		}
-		
+		$.initContextUrl();
 		
 	});
 });
