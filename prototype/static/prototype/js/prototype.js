0e2c6a99460cbe9185195bae42e2c569bf301706
Aiden Choi <ethdemor@gmail.com>
2015-06-26 18:35:31 +0900

    키보드 내비게이션.

--- prototype/static/prototype/js/prototype.js
+++ prototype/static/prototype/js/prototype.js
@@ -1,3 +1,48 @@
 /**
  * Created by moer on 15. 6. 16..
  */
+
+$(document).ready(function() {
+
+
+    Mousetrap.bind('right', function() {
+        var $currentItem = $('.focus'),
+            $nextItem = $currentItem.next('.item');
+
+        if ($nextItem.size() > 0) {
+            $currentItem.removeClass('focus');
+            $nextItem.addClass('focus');
+        }
+    });
+
+    Mousetrap.bind('left', function() {
+        var $currentItem = $('.focus'),
+            $prevItem = $currentItem.prev();
+
+        if ($prevItem.size() > 0) {
+            $currentItem.removeClass('focus');
+            $prevItem.addClass('focus');
+        }
+    });
+
+    Mousetrap.bind('up', function() {
+       var $currentItem = $('.focus'),
+           $upperRowItem = $currentItem.parent().prev().children().first();
+
+        if ($upperRowItem.size() > 0) {
+            $currentItem.removeClass('focus');
+            $upperRowItem.addClass('focus');
+        }
+    });
+
+    Mousetrap.bind('down', function() {
+       var $currentItem = $('.focus'),
+           $bottomRowItem = $currentItem.parent().next().children().first();
+
+        if ($bottomRowItem.size() > 0) {
+            $currentItem.removeClass('focus');
+            $bottomRowItem.addClass('focus');
+        }
+    });
+
+});
